ddc7e795c49680cb035eaf38aca4931a98437953
paul <soapmactavish799@gmail.com>
2020-07-21 12:15:51 +0300

    Setup test environment

---
+++ portfolio/src/test/java/com/google/sps/servlets/CommentsServletTest.java
@@ -0,0 +1,16 @@
+package com.google.sps.servlets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+
+class CommentsServletTest {
+  private CommentsServlet servlet;
+  private MockHttpServletRequest request;
+  private MockHttpServletResponse response;
+  
+  @Test
+  void testPost() {
+
+  }
+}
