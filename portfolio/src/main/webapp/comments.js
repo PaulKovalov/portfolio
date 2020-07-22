fe2b4613b0ba9652282bdc4ba220a15b2cef532a
paul <soapmactavish799@gmail.com>
2020-07-22 13:34:53 +0300

    Add working comments fetching

--- portfolio/src/main/webapp/comments.js
+++ portfolio/src/main/webapp/comments.js
@@ -4,13 +4,14 @@ async function fetchComments() {
   const commentsJson = await response.json();
   // create a hash table of type "comment id" -> "comment" to build a comment tree
   const commentsMap = {};
-  for (const c in commentsJson) {
-    commentsMap[c.key] = c;
+  for (const i in commentsJson) {
+    const comment = commentsJson[i];
+    commentsMap[comment.key] = comment;
   }
-  console.log(commentsMap);
-  for (const c in commentsMap) {
-    if (!c.replyTo) {
-      // means comment is a root of the comment branch, start DFS from here
+  // build a tree, starting DFS from the vertex (comments) that have no incoming vertexes (are not replies)
+  for (const i in commentsMap) {
+    const comment = commentsMap[i];
+    if (!comment.replyTo) {
       buildCommentsTree(commentsMap, comment, 0);
     }
   }
@@ -20,7 +21,8 @@ function buildCommentsTree(commentsMap, comment, depth) {
   // add current comment to dom
   addCommentToDOM(comment, depth)
   // iterate over all replies in the map, add them as the children of the current comment
-  for (const replyId in comment.replies) {
+  for (const i in comment.replies) {
+    const replyId = comment.replies[i];
     const reply = commentsMap[replyId]
     buildCommentsTree(commentsMap, reply, Number(depth + 1));
   }
@@ -28,17 +30,19 @@ function buildCommentsTree(commentsMap, comment, depth) {
 
 function addCommentToDOM(comment, depth) {
   // creates a div with two paragraph, one for username and one for comment text
-  const commentDOMElement = document.createElement("div");
-  const commentAuthorUsername = document.createElement("p");
+  const commentDOMElement = document.createElement('div');
+  const commentAuthorUsername = document.createElement('h5');
   commentAuthorUsername.innerText = comment.username;
-  const commentText = document.createElement("p");
+  const commentText = document.createElement('p');
   commentText.innerText = comment.text;
-  commentDOMElement.style = "margin-left: " + sqrt(Number(depth * 10)) + "px";
+  commentDOMElement.style = 'margin-left: ' + Math.sqrt(Number(depth * 10)) + 'px';
   commentDOMElement.appendChild(commentAuthorUsername);
   commentDOMElement.appendChild(commentText);
+  commentDOMElement.classList.add('comment')
   // next I need a form for posting a reply to this comment
-  const replyForm = document.createElement("form");
-  replyForm.action = "/comments"
-  replyForm.method = "POST"
-
+  const replyForm = document.createElement('form');
+  replyForm.action = '/comments';
+  replyForm.method = 'POST';
+  // attach created comment to the comments div
+  document.getElementById('comments').appendChild(commentDOMElement);
 }
